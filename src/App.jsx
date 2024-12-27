68fdadeac1ff85744bd6e3bb44f20c02cbd562e8
sangeeth911 <sangeeth999123@gmail.com>
2024-12-27 17:53:46 +0530

    remove unused components and add json-server for local data handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,15 @@
-import Friendslist from "./eat-n-split/Friendslist";
-import Frndaddform from "./eat-n-split/Frndaddform";
-import SplitForm from "./eat-n-split/SplitForm";
-import { useState } from "react";
-import "./App.css";
 
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933472",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=930372",
-    balance: 0,
-  },
-];
 
 function App() {
-  const [friends, setFriends] = useState(initialFriends);
-  const [showaddform, setShowaddform] = useState(false);
-  const [selectedFriend, setSelectedFriend] = useState(null);
-
-  function handleshowaddfrnd() {
-    setShowaddform(!showaddform);
-  }
-
-  function handleaddfriend(friend) {
-    setFriends((friends) => [...friends, friend]);
-    setShowaddform(false);
-  }
-
-  function handleselectfriend(friend) {
-    setSelectedFriend(friend);
-  }
-
-  function handleSplitBill(amount, payer) {
-    setFriends((currentFriends) =>
-      currentFriends.map((friend) => {
-        if (friend.id === selectedFriend.id) {
-          return {
-            ...friend,
-            balance:
-              payer === "you"
-                ? friend.balance + amount
-                : friend.balance - amount,
-          };
-        }
-        return friend;
-      })
-    );
-    setSelectedFriend(null);
-  }
-
+ 
   return (
-    <div className="app">
-      <div className="sidebar">
-        <Friendslist friends={friends} onSelection={handleselectfriend} />
-        {showaddform && <Frndaddform onaddfriends={handleaddfriend} />}
-
-        <button
-          onClick={handleshowaddfrnd}
-          style={{
-            backgroundColor: "#4CAF50",
-            color: "white",
-            padding: "10px 20px",
-            border: "none",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}
-        >
-          {showaddform ? "Close" : "Add Friend"}
-        </button>
-      </div>
-      {selectedFriend && (
-        <SplitForm
-          selectedFriend={selectedFriend}
-          onSplitBill={handleSplitBill}
-        />
-      )}
-    </div>
+    
+    <>
+    
+    
+    </>
   );
 }
 
 export default App;
+ 
--- src/Billinput.jsx
+++
@@ -1,9 +0,0 @@
-// // import React from 'react'
-
-// const Billinput = () => {
-//   return (
-//     <div>Billinput</div>
-//   )
-// }
-
-// export default Billinput
