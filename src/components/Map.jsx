40d4601e5c72352676b3e0da99b8406342018312
ZAPE <sangeeth999123@gmail.com>
2025-01-01 02:17:26 +0530

    Update Map component to integrate Leaflet for map rendering. Modify styles for map container and enhance layout for better responsiveness. Upgrade React and React-DOM to version 19 and add react-leaflet dependency.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,31 @@
-// import React from 'react'
-// import {  useSearchParams } from 'react-router-dom'
-import styles from './Map.module.css'
-import { useNavigate } from 'react-router-dom'
+// filepath: /c:/Users/sange/Documents/WORLDspace/src/components/Map.jsx
+// import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import styles from "./Map.module.css";
+
 function Map() {
-const navigate=useNavigate()
-  // const [SearchParams,setsearchparams]= useSearchParams()
-  // const lat=SearchParams.get('lat')
-  // const lng=SearchParams.get('lng')
+  const mapPosition = [51.505, -0.09];
+
   return (
-    <div className={styles.mapContainer}
-      onClick={()=>
-        navigate("form")
-      }
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-      {/* <h1>{lat}</h1> */}
-      {/* <h3>{lng}</h3> */}
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            <span><h1>jjj</h1></span>
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
-  )
+  );
 }
 
-export default Map
+export default Map;
