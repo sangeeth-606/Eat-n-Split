9caa1f35496db42b633680ff956e8f2068625c6a
ZAPE <sangeeth999123@gmail.com>
2025-01-01 19:53:47 +0530

    Add geolocation support to Map component and create useGeolocation hook. Implement button to fetch current position and update map center accordingly. Refactor code for improved readability.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -54,30 +54,53 @@
 /* eslint-disable react/no-unknown-property */
 // filepath: /c:/Users/sange/Documents/WORLDspace/src/components/Map.jsx
 // import React from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import styles from "./Map.module.css";
 import { useCities } from "./contexts/CitiesContext";
 import { useSearchParams } from "react-router-dom";
+import Button from "./Button";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useGeolocation } from "../hooks/useGeolocation";
 
 function Map() {
   const [mapPosition, setmapPosition] = useState([51.505, -0.09]);
   const { cities } = useCities();
 
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocation,
+    getPosition,
+  } = useGeolocation();
+  const mapLat = parseFloat(searchParams.get("lat")) || 40;
+  const mapLng = parseFloat(searchParams.get("lng")) || 0;
 
-  const mapLat = parseFloat(searchParams.get("lat")) || 40; 
-  const mapLng = parseFloat(searchParams.get("lng")) || 0; 
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setmapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
-
-  useEffect(function(){
-    if (mapLat && mapLng)
-    setmapPosition([mapLat , mapLng])
-  }, [mapLat, mapLng])
+  useEffect(() => {
+    if (geolocation) setmapPosition([geolocation.lat, geolocation.lng]);
+  }, [geolocation]);
 
   return (
     <div className={styles.mapContainer}>
+      { 
+      !geolocation &&(
+       <Button type="position" onClick={getPosition}>
+        {isLoadingPosition ? "Loading..." : "Use Current Position"}
+      </Button>)}
       <MapContainer
         center={mapPosition}
         zoom={6}
@@ -113,11 +136,11 @@ function ChangeCentre({ position }) {
   return null;
 }
 
-function DetectClick(){
+function DetectClick() {
   const navigate = useNavigate();
   useMapEvents({
-    click : (e)=> navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
-  })
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+  });
 }
- 
+
 export default Map;
