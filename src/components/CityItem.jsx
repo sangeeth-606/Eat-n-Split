73cc9a928c71f268d3702d89b12312179c444db9
ZAPE <sangeeth999123@gmail.com>
2025-01-03 00:52:03 +0530

    Done

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-// import React from 'react'
+
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 // import { use } from "react";
